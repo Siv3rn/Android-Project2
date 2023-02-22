6f18a6edb6e5aee629eebd46e3e1c473e5e44321
Sivern <anggito732@gmail.com>
2023-02-22 09:09:31 +0700

    main

--- app/src/main/java/com/example/fragmentexample1updated/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample1updated/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.fragmentexample1updated;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1;
+    private boolean display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (display){
+                    displayFragment();
+                }
+                else{
+                    closeFragment();
+                }
+            }
+        });
+
+    }
+
+    private void closeFragment() {
+    }
+
+    private void displayFragment() {
     }
 }
